a2b278ae4872eeb7c5aa50f819244cb3929f4d2c
William Maphanga <williesdk@gmail.com>
2019-10-06 10:54:13 +0200

    modified great places provider
    fix: table name

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+
 import '../models/place.dart';
 import '../helpers/db_helper.dart';
 
@@ -11,20 +12,37 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  addPlace(String title, File image) {
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
     final newPlace = Place(
       id: DateTime.now().toString(),
-      image: image,
-      title: title,
+      image: pickedImage,
+      title: pickedTitle,
       location: null,
     );
-
     _items.add(newPlace);
     notifyListeners();
-    DBHelper.insert('places', {
+    DBHelper.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
-      'image': newPlace.image,
+      'image': newPlace.image.path,
     });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
   }
 }
